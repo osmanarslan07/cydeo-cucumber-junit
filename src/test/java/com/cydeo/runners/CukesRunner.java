d564f88e4bbc9b59fae13f651534ad923f4b39ae
osmanarslan07 <osmanarslan07@yahoo.com>
2023-02-18 21:25:31 +0300

    Cucumber Intro

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,9 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = true
+        dryRun = false,
+        tags = "@employee and not @admin"
 )
 public class CukesRunner {
 }
