6936c31fa4e05f814b753350e3a860a898fa86d3
osmanarslan07 <osmanarslan07@yahoo.com>
2023-02-19 21:45:57 +0300

    day 16 - GoogleStepDefinitions.java take screen shots

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -6,10 +6,7 @@ In the class we will be able to pass pre- & post- conditions to each scenario an
 
 
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import io.cucumber.java.*;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -34,9 +31,12 @@ public class Hooks {
 
 
     @After
-    public void teardownScenario(){
+    public void teardownScenario(Scenario scenario){
+
+
 
         byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot,"image/png", scenario.getName());
 
 
 
