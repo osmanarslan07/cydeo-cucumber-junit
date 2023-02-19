b147888e4f49f6c216026c6e2b715aa700871cca
osmanarslan07 <osmanarslan07@yahoo.com>
2023-02-19 18:58:57 +0300

    Cucumber Practices

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -13,10 +13,22 @@ import io.cucumber.java.BeforeStep;
 public class Hooks {
 
     //import from io.cucumber.java not from junit
-    @Before
+    @Before(order = 1)
     public void setupScenario(){
         System.out.println("====Setting up browser before using cucumber @Before");
     }
+
+    @Before (value = "@login", order = 2)
+    public void setupScenarioForLogins(){
+        System.out.println("====this will only apply to scenarios with @login tag");
+    }
+
+    @Before (value = "@db", order = 0)
+    public void setupForDatabaseScenarios(){
+        System.out.println("====this will only apply to scenarios with @db tag");
+    }
+
+
 
     @After
     public void teardownScenario(){
