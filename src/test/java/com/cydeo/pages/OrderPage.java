8196079fcb1f4989ccb396edf19c280f3e42f748
osmanarslan07 <osmanarslan07@yahoo.com>
2023-02-26 18:11:02 +0300

    day 17 - Order_StepDefinitions.java

---
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -0,0 +1,57 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderPage extends BasePage {
+
+    public OrderPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(name = "product")
+    public WebElement productDropdown;
+
+    @FindBy(name = "quantity")
+    public WebElement inputQuantity;
+
+    @FindBy(name = "name")
+    public WebElement inputName;
+
+    @FindBy(name = "street")
+    public WebElement inputStreet;
+
+    @FindBy(name = "city")
+    public WebElement inputCity;
+
+    @FindBy(name = "state")
+    public WebElement inputState;
+
+    @FindBy(name = "zip")
+    public WebElement inputZip;
+
+    @FindBy(name = "cardNo")
+    public WebElement inputCardNo;
+
+    @FindBy(name = "cardExp")
+    public WebElement inputCardExp;
+
+    @FindBy(xpath = "//input[@value='American Express']")
+    public WebElement inputCard;
+
+    @FindBy(xpath = "//button[.='Process Order']")
+    public WebElement processOrderButton;
+
+    @FindBy(xpath = "//table//td[1]")
+    public WebElement tableFirstName;
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/Order_StepDefinitions.java
@@ -1,62 +1,89 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.BasePage;
+import com.cydeo.pages.OrderPage;
+import com.cydeo.pages.WebTableLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 
 public class Order_StepDefinitions {
 
+    WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+    BasePage basePage = new BasePage();
+    OrderPage orderPage = new OrderPage();
 
     @Given("user is already logged in and on order page")
     public void user_is_already_logged_in_and_on_order_page() {
+        //getting the page using URL from configuration.properties
+        Driver.getDriver().get(ConfigurationReader.getProperty("web.table.url"));
+
+        //calling our login method to log in to web table app
+        webTableLoginPage.login();
+
+        //clicking to "order" link to go order page
+        basePage.order.click();
          
     }
+
     @When("user selects product type {string}")
     public void user_selects_product_type(String string) {
-         
+        Select select = new Select(orderPage.productDropdown);
+        select.selectByVisibleText(string);
     }
-    @When("user enters quantity {int}")
-    public void user_enters_quantity(Integer int1) {
-         
+    @When("user enters quantity {string}")
+    public void user_enters_quantity(String string) {
+        orderPage.inputQuantity.sendKeys(Keys.BACK_SPACE);
+        orderPage.inputQuantity.sendKeys(string);
     }
     @When("user enters customer name {string}")
     public void user_enters_customer_name(String string) {
-         
+         orderPage.inputName.sendKeys(string);
     }
     @When("user enters street {string}")
     public void user_enters_street(String string) {
-         
+         orderPage.inputStreet.sendKeys(string);
     }
     @When("user enters city {string}")
     public void user_enters_city(String string) {
-         
+         orderPage.inputCity.sendKeys(string);
     }
     @When("user enters state {string}")
     public void user_enters_state(String string) {
-         
+         orderPage.inputState.sendKeys(string);
     }
     @When("user enters zipcode {string}")
     public void user_enters_zipcode(String string) {
-         
+         orderPage.inputZip.sendKeys(string);
     }
     @When("user selects credit card type {string}")
     public void user_selects_credit_card_type(String string) {
-         
+         orderPage.inputCard.click();
     }
     @When("user enters credit card number {string}")
     public void user_enters_credit_card_number(String string) {
-         
+         orderPage.inputCardNo.sendKeys(string);
     }
     @When("user enters expiry date {string}")
     public void user_enters_expiry_date(String string) {
-         
+         orderPage.inputCardExp.sendKeys(string);
     }
     @When("user enters process order button")
     public void user_enters_process_order_button() {
-         
+         orderPage.processOrderButton.click();
     }
     @Then("user should see {string} in first row of the web table")
     public void user_should_see_in_first_row_of_the_web_table(String string) {
+        basePage.viewAllOrders.click();
+        String actualFirstNameInTable = orderPage.tableFirstName.getText();
+
+        Assert.assertTrue(actualFirstNameInTable.contains(string));
          
     }
 
